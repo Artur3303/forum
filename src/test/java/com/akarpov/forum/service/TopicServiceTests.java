99c4f8b8dc32772c9cc512ba7a2ed591ad6acdb9
Artur Karpov <artik100313@gmail.com>
2020-01-17 23:47:00 +0300

    added TopicService tests
    findAll should return topic list
    findAll should invoke TopicRepository#findAll

---
+++ src/test/java/com/akarpov/forum/service/TopicServiceTests.java
@@ -0,0 +1,47 @@
+package com.akarpov.forum.service;
+
+import com.akarpov.forum.entity.Topic;
+import com.akarpov.forum.repository.TopicRepository;
+import com.akarpov.forum.service.impl.TopicServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = {TopicServiceImpl.class})
+class TopicServiceTests {
+
+    @Autowired
+    private TopicService topicService;
+
+    @MockBean
+    private TopicRepository topicRepository;
+
+    @Test
+    public void contextLoads() {
+        assertThat(topicService).isNotNull();
+    }
+
+    @Test
+    public void findAllShouldReturnTopicList() {
+        List<Topic> expected = Arrays.asList(new Topic(1L, "A"), new Topic(2L, "B"));
+        when(topicRepository.findAll()).thenReturn(expected);
+        List<Topic> actual = topicService.findAll();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllShouldInvokeTopicRepository_findAll() {
+        topicService.findAll();
+        verify(topicRepository).findAll();
+    }
+
+}
