b3b97afc76476813944cfad53e04e6c31d8dbbda
Artur Karpov <artik100313@gmail.com>
2020-01-23 21:38:03 +0300

    fixed creating user

--- src/main/java/com/akarpov/forum/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/akarpov/forum/service/impl/AppUserServiceImpl.java
@@ -2,6 +2,7 @@ package com.akarpov.forum.service.impl;
 
 import com.akarpov.forum.dto.UserDto;
 import com.akarpov.forum.entity.AppUser;
+import com.akarpov.forum.entity.Role;
 import com.akarpov.forum.repository.UserRepository;
 import com.akarpov.forum.service.AppUserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,6 +31,7 @@ public class AppUserServiceImpl implements AppUserService {
                 .encryptedPassword(passwordEncoder.encode(userDto.getPassword()))
                 .firstName(userDto.getFirstName())
                 .lastName(userDto.getLastName())
+                .role(Role.USER)
                 .build();
         return userRepository.save(user);
     }
